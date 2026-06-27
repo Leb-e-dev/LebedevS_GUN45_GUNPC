dd85bce14ea1b5409bb5315fd2289ce8bb2483d8
Lebedev <axolotie28@gmail.com>
2026-06-27 18:38:38 +0300

    Cycles homework

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 namespace HomeWork
 {
@@ -6,43 +6,41 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            int[] fibonacci = { 0, 1, 1, 2, 3, 5, 8, 13 };
+            int first = 0;
+            int second = 1;
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (int i = 2; i < 10; i++)
+            {
+                int next = first + second;
+                Console.WriteLine(next);
+                first = second;
+                second = next;
+            }
 
-            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            for (int i = 2; i <= 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
 
-            int[,] matrix = {
-                { 2, 3, 4 },
-                { 4, 9, 16 },
-                { 8, 27, 64 }
-            };
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write((i * j) + "\t");
+                }
+                Console.WriteLine();
+            }
 
-            double[][] jagged = {
-                new double[] { 1, 2, 3, 4, 5 },
-                new double[] { Math.E, Math.PI },
-                new double[] { Math.Log10(1), Math.Log10(10), Math.Log10(100), Math.Log10(1000) }
-            };
-
-            int[] array = { 1, 2, 3, 4, 5 };
-            int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
-            var result = CopyArrays(array, array2, 3);
-
-            Console.WriteLine(string.Join(" ", result));
-
-            string[] sample = { "", "" };
-            ResizeArray(ref array, array.Length * 2);
-
-            Console.WriteLine(string.Join(" ", array));
-        }
-
-        static int[] CopyArrays(int[] source, int[] destination, int count)
-        {
-            Array.Copy(source, 0, destination, 0, count);
-            return destination;
-        }
-
-        static void ResizeArray(ref int[] sourceArray, int newSize)
-        {
-            Array.Resize(ref sourceArray, newSize);
+            string password = "qwerty";
+            string? input;
+            do
+            {
+                Console.Write("Enter password: ");
+                input = Console.ReadLine();
+            }
+            while (input != password);
+            Console.WriteLine("Access granted!");
         }
     }
 }
